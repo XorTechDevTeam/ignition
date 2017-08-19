7b892710d48681741541459c05b31dd41a110642
Rabbitlancer <slendercrow@mail.ru>
2017-08-19 23:55:52 +0300

    Issue #14, Issue #27: preparing to merge and documenting

--- src/Core/XtMetadata.h
+++ src/Core/XtMetadata.h
@@ -13,35 +13,83 @@
 #include <XtCommon.h>
 
 namespace xt {
-    enum PlatformType {
-        PTLinux,
-        PTOSX,
-        PTWindows32,
-        PTWindowsUWP,
-        PTAndroid,
-        PTiOS,
-        PTEmscripten
+    /**
+     * Тип платформы, на которой работает XorTech.
+     */
+    enum XtPlatformType {
+        PTLinux,        /**< Да ладно, это линух! */
+        PTOSX,          /**< Да ладно, это макось! */
+        PTWindows32,    /**< Да ладно, это винда32! */
+        PTWindowsUWP,   /**< Да ладно, это виндомаркет! */
+        PTAndroid,      /**< Да ладно, это ведроид! */
+        PTiOS,          /**< Да ладно, это яОСь! */
+        PTEmscripten    /**< А это мы в вебе. */
     };
 
+    /**
+     * Класс метаданных о текущей сборке движка XorTech.
+     */
     class XtMetadata {
     private:
-        PlatformType platformName;
-        String subplatformName;
-        int platformVerMajor;
-        int platformVerMinor;
-        int xorTechVerMajor;
-        int xorTechVerMinor;
+        XtPlatformType platformName;    /**< Тип платформы.
+                                         * @see XtPlatformType
+                                         */
+        String subplatformName;         /**< Подтип платформы.
+                                         * В Linux это дистрибутив.
+                                         * В Windows - версия Windows.
+                                         * В Emscripten - текующий User-Agent.
+                                         */
+        int platformVerMajor;           /**< Старшая версия платформы. */
+        int platformVerMinor;           /**< Младшая версия платформы. */
+        int xorTechVerMajor;            /**< Старшая версия сборки XorTech. */
+        int xorTechVerMinor;            /**< Младшая версия сборки XorTech. */
         //TODO: modules
 
+        /**
+         * Собирает информацию о текущей сборке.
+         */
         void init();
     public:
+        /**
+         * Публичный конструктор метаданных.
+         * Сразу после создания объект содержит в себе все данные и годен к использованию.
+         */
         XtMetadata();
 
-        PlatformType getPlatformName() const;
+        /**
+         * Метод-геттер типа платформы.
+         * @return Возвращает тип платформы.
+         */
+        XtPlatformType getPlatformName() const;
+
+        /**
+         * Метод-геттер подтипа платформы.
+         * @return Возвращает строку-подтип платформы.
+         */
         const String &getSubplatformName() const;
+
+        /**
+         * Метод-геттер старшей версии платформы.
+         * @return Возвращает число: старшую версию платформы.
+         */
         int getPlatformVerMajor() const;
+
+        /**
+         * Метод-геттер младшей версии платформы.
+         * @return Возвращает число: младшую версию платформы.
+         */
         int getPlatformVerMinor() const;
+
+        /**
+         * Метод-геттер старшей версии сборки.
+         * @return Возвращает число: старшую версию сборки.
+         */
         int getXorTechVerMajor() const;
+
+        /**
+         * Метод-геттер младшей версии сборки.
+         * @return Возвращает число: младшую версию сборки.
+         */
         int getXorTechVerMinor() const;
     };
 }
